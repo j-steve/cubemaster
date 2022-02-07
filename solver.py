970e95ad434ff84189539d00ac2edc18fd3364da
Stephen <steve5805@gmail.com>
2022-02-07 12:07:48 -0500

    Replaces risk threshold with exhaustive tree search

--- solver.py
+++ solver.py
@@ -6,7 +6,8 @@ import random
 
 _ROTATIONS = [Rotation.LEFT, Rotation.UP, Rotation.COUNTERCLOCKWISE]
 _OFFSETS = [0, 1, 2]
-_MAX_UNIFORMITY_SCORE =  108 #72 #102
+_MAX_UNIFORMITY_SCORE = 108  # 72 #102
+_ROTATIONS_PER_STEP = 15
 
 
 class Solver(object):
@@ -22,60 +23,50 @@ class Solver(object):
     def solve(self):
         print('Solving...')
         # steps = []
-        steps = 0
-        uniformity_score = self.cube.get_uniformity_score()
-        initial_uniformity_score = uniformity_score
-        best_uniformity_score = uniformity_score
+        initial_uniformity_score = self.cube.get_uniformity_score()
+        best_uniformity_score = initial_uniformity_score
         reverts = 0
         iterations = 0
         total_rotate_calc_time = 0
         risk_tolerance = 0.0
-        cube_copy = copy.deepcopy(self.cube)
-        while steps < 1000000000000000:
 
-            start_time = time.time()
-            #risk_tolerance = random.random()  # likelihood of choosing a worse or neutral option vs a net improvement
-            if risk_tolerance < 1:
-                risk_tolerance += 0.000001
-            #if len(self._permutations_to_skip) >= 9:
-             #   risk_tolerance += 0.03
-              #  self._permutations_to_skip.clear()
-
-            #this_rotation = self._random_rotation()
-            this_rotation = None
-            for i in range(0, 20):
-                self._random_rotation()
-            total_rotate_calc_time += time.time() - start_time
-            new_uniformity_score = self.cube.get_uniformity_score()
+        while iterations < 1000000000000000:
             iterations += 1
-
-            if (new_uniformity_score < uniformity_score and risk_tolerance < 0.8) or (
-                    new_uniformity_score == uniformity_score and risk_tolerance < 0.6):
-                self._permutations_to_skip.add(this_rotation)
-                self.cube = cube_copy
-                reverts += 1
-            else:
-                cube_copy = copy.deepcopy(self.cube)
-                risk_tolerance -= 0.5
-                self._permutations_to_skip.clear()
-                if new_uniformity_score > uniformity_score:
-                    risk_tolerance = 0
-                    if new_uniformity_score > best_uniformity_score:
-                        best_uniformity_score = new_uniformity_score
-                uniformity_score = new_uniformity_score
-                steps += 1
-
-            if iterations % 10000 == 0:
+            ranked_cubes = {}
+            for cube in self._rotate_x_times(self.cube, rotations=0, target_rotations=_ROTATIONS_PER_STEP):
+                uniformity_score = cube.get_uniformity_score()
+                if uniformity_score not in ranked_cubes:
+                    ranked_cubes[uniformity_score] = []
+                ranked_cubes[uniformity_score].append(cube)
+            iteration_uniformity_score = max(ranked_cubes.keys())
+            self.cube = ranked_cubes[iteration_uniformity_score][0]
+            if iteration_uniformity_score > best_uniformity_score:
+                best_uniformity_score = iteration_uniformity_score
+            if iterations % 1 == 0:
                 print(
-                    'Iteration #%s (uniformity %s | best %s | initial %s | risk tolerance %i.1%% | %s reverts | avg calc time: %.2gms)' % (
-                        steps, uniformity_score, best_uniformity_score, initial_uniformity_score, risk_tolerance * 100, reverts,
-                        total_rotate_calc_time / iterations * 1000))
+                    'Rotation #%s (uniformity %i%% | best %i%% | initial %i%%)' % (
+                        iterations * _ROTATIONS_PER_STEP, iteration_uniformity_score / _MAX_UNIFORMITY_SCORE * 100,
+                        best_uniformity_score / _MAX_UNIFORMITY_SCORE * 100,
+                        initial_uniformity_score / _MAX_UNIFORMITY_SCORE * 100))
                 self.cube.print()
             if uniformity_score == _MAX_UNIFORMITY_SCORE:
-                print("Solved in %s", steps)
+                print("Solved in %s", iterations)
+                self.cube.print()
                 return
         print('Failed to solve after 1000000000000000 iterations.')
-        cube.print()
+        self.cube.print()
+
+    def _rotate_x_times(self, cube: Cube, rotations: int, target_rotations: int):
+        for rotation in _ROTATIONS:
+            for offset in _OFFSETS:
+                cube_copy = copy.deepcopy(cube)
+                cube_copy.rotate(rotation, offset)
+                rotations += 1
+                if rotations < target_rotations:
+                    for child_cube in self._rotate_x_times(cube_copy, rotations, target_rotations):
+                        yield child_cube
+                else:
+                    yield cube_copy
 
     def _random_rotation(self):
         result = None
